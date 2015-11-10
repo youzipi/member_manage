a06c300a6aad25e5d144d570bfd145addae19548
youzipi <blake_12@qq.com>
2015-11-10 19:54:47 +0800

    add some common methods

---
+++ src/main/java/com/controller/BaseController.java
@@ -0,0 +1,13 @@
+package com.controller;
+
+/**
+ * project_name:member_manage
+ * package_name:com.controller
+ * user: youzipi
+ * date: 15-11-10 下午7:14
+ */
+public class BaseController {
+    protected String redirect(String url) {
+        return "redirect:" + url;
+    }
+}
