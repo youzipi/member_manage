77bcd2f1b9a457003f3fec7a45e672b9065268e9
youzipi <blake_12@qq.com>
2015-11-13 12:46:40 +0800

    remove useless

--- src/main/java/com/controller/HomeController.java
+++ src/main/java/com/controller/HomeController.java
@@ -1,11 +1,7 @@
 package com.controller;
 
-import com.dao.UserDao;
 import com.entity.User;
-import com.mapper.TestMapper;
-import com.mapper.UserMapper;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import com.dao.UserDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
@@ -23,21 +19,14 @@ import java.util.List;
  * date: 15-11-8 下午12:51
  */
 @Controller
-@RequestMapping("/")
+@RequestMapping("/home")
 public class HomeController {
     private static int counter = 0;
-    private static final String VIEW_INDEX = "index";
-    private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+
 
     @Autowired
-    private UserDao userDao;
-    @Autowired
-    private UserMapper userMapper;
-    @Autowired
-    private TestMapper testMapper;
-//
-//    @Autowired
-//    private UserService userService;
+    private UserDao userMapper;
+
 
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
@@ -50,8 +39,8 @@ public class HomeController {
     }
 
     @RequestMapping(value = "/{name}", method = RequestMethod.GET)
-    public String welcome(@PathVariable String name, ModelMap model) {
-
+    public String welcome2(@PathVariable String name, ModelMap model) {
         return "index";
     }
+
 }
